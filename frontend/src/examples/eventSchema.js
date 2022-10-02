0f63e5de1719a7b8e4352c37b90c225be2774685
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-10-02 17:47:34 +0900

    added new directory that represents the schema

---
+++ frontend/src/examples/eventSchema.js
@@ -0,0 +1,9 @@
+export const newEvent =
+  {
+    title: "Project Success POST without ID",
+    details: "Working on the project to create todo app",
+    start: "2022-10-07T23:08:00Z",
+    end: "2022-10-20T23:08:00Z",
+    story_points: 3,
+    status: "open",
+  };
