a4b6002d6154e7dbd2ffec4c3d82193372578332
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-10-02 17:48:54 +0900

    added router

---
+++ frontend/src/router/BaseRouter.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "../components/pages/Home";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
+
+// import { Protected } from "../components/molecules/Protected";
+// import { Account } from "../components/pages/Account";
+// import { Board } from "../components/pages/Board";
+// import { Home } from "../components/pages/Home";
+// import { SignIn } from "../components/pages/SignIn";
+import { Tasks } from "../components/pages/Tasks";
+// import { Stats } from "../components/pages/Stats";
+// import { HeaderLayout } from "../components/templates/HeaderLayout";
+
+export const BaseRoute = () => {
+  return (
+    <BrowserRouter>
+      <HeaderLayout />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/signin" element={<SignIn />} /> */}
+        {/* <Route
+          path="/account"
+          element={
+            <Protected>
+              <Account />
+            </Protected>
+          }
+        /> */}
+        {/* <Route path="/board" element={<Board />} /> */}
+        <Route path="/tasks" element={<Tasks />} />
+        {/* <Route path="/stats" element={<Stats />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+};
