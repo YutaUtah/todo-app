73fa0228a0e18e0a79290dba2ab7cdc76ce38493
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-10-02 17:57:30 +0900

    modified so cards will be reflected

--- frontend/src/components/organisms/todoBox/HorinzontalToDoBox.jsx
+++ frontend/src/components/organisms/todoBox/HorinzontalToDoBox.jsx
@@ -5,31 +5,32 @@ import { Grid, GridItem } from "@chakra-ui/react";
 import { ToDoCard } from "../../molecules/ToDoCard";
 
 export const HorizontalToDoBox = memo((props) => {
-  const { displayedEvents, setDisplayedEvents, setAllEvents } = props;
+  const { allEvents, displayedEvents, setDisplayedEvents, setAllEvents } = props;
 
-  const deleteToDoCard = (event) => {
-    const modifiedAfterDeletedEvents = displayedEvents.filter(
-      (eachEvent) => eachEvent.title !== event.title
-    );
-    setDisplayedEvents(modifiedAfterDeletedEvents);
-    setAllEvents(modifiedAfterDeletedEvents);
-  };
+//   const deleteToDoCard = (event) => {
+//     const modifiedAfterDeletedEvents = displayedEvents.filter(
+//       (eachEvent) => eachEvent.title !== event.title
+//     );
+//     setDisplayedEvents(modifiedAfterDeletedEvents);
+//     setAllEvents(modifiedAfterDeletedEvents);
+//   };
 
-  const changeStatus = (status, title) => {
-    // Deepcopied with the spread syntax
-    const modifiedStatusEvents = [...displayedEvents];
-    modifiedStatusEvents.map((eachEvent) => {
-      if (eachEvent.title === title) {
-        eachEvent.status = status;
-      }
-    });
-    setDisplayedEvents(modifiedStatusEvents);
-  };
+//   const changeStatus = (status, title) => {
+//     // Deepcopied with the spread syntax
+//     const modifiedStatusEvents = [...displayedEvents];
+//     modifiedStatusEvents.map((eachEvent) => {
+//       if (eachEvent.title === title) {
+//         eachEvent.status = status;
+//       }
+//     });
+//     setDisplayedEvents(modifiedStatusEvents);
+//   };
+console.log(allEvents)
 
   return (
     <>
       <Grid templateColumns="repeat(4, 1fr)" mt={6} mb={5}>
-        {displayedEvents.map((event) => (
+        {allEvents.map((event) => (
           <GridItem w="100%" key={event.title}>
             <ToDoCard
               key={event.title}
@@ -39,8 +40,8 @@ export const HorizontalToDoBox = memo((props) => {
               details={event.details}
               status={event.status}
               storyPoints={event.storyPoints}
-              deleteToDoCard={() => deleteToDoCard(event)}
-              changeStatus={(status, title) => changeStatus(status, title)}
+            //   deleteToDoCard={() => deleteToDoCard(event)}
+            //   changeStatus={(status, title) => changeStatus(status, title)}
             />
           </GridItem>
         ))}
