25d655feb2f29cb39064635788e3efa2a4b3a24a
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-10-03 23:39:54 +0900

    all router

--- frontend/src/router/BaseRouter.jsx
+++ frontend/src/router/BaseRouter.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-// import { Protected } from "../components/molecules/Protected";
-// import { Account } from "../components/pages/Account";
+import { Protected } from "../components/molecules/Protected";
+import { Account } from "../components/pages/Account";
 import { Board } from "../components/pages/Board";
 import { Home } from "../components/pages/Home";
-// import { SignIn } from "../components/pages/SignIn";
+import { SignIn } from "../components/pages/SignIn";
 import { Tasks } from "../components/pages/Tasks";
 import { Stats } from "../components/pages/Stats";
 import { HeaderLayout } from "../components/templates/HeaderLayout";
@@ -16,15 +16,15 @@ export const BaseRoute = () => {
       <HeaderLayout />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/signin" element={<SignIn />} /> */}
-        {/* <Route
+        <Route path="/signin" element={<SignIn />} />
+        <Route
           path="/account"
           element={
             <Protected>
               <Account />
             </Protected>
           }
-        /> */}
+        />
         <Route path="/board" element={<Board />} />
         <Route path="/tasks" element={<Tasks />} />
         <Route path="/stats" element={<Stats />} />
