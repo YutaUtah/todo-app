680031650c5c7e0f7a1d4d9191719be772be2c89
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-10-03 23:39:01 +0900

    added model choices

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -9,8 +9,20 @@ class Todo(models.Model):
     details = models.TextField(max_length=120, null=False)
     start = models.DateTimeField(help_text='Start Date')
     end = models.DateTimeField(help_text='End Date')
-    story_points = models.IntegerField(default=0, null=False, validators=[MinValueValidator(0), MaxValueValidator(100)])
-    status = models.CharField(max_length=10, null=False)
+    story_points = models.IntegerField(default=0, null=False, validators=[
+                                       MinValueValidator(0), MaxValueValidator(100)])
+
+    class Status(models.TextChoices):
+        OPEN = 'open'
+        INPROGRESS = 'in progress'
+        DONE = 'done'
+
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.OPEN,
+        null=False,
+    )
 
     def __str__(self):
         return self.title
